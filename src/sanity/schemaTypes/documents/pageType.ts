49e126624586c0800f61d61927287cc7d861fc7a
Raúl Garcia <ego.war@icloud.com>
2025-05-25 05:58:26 -0700

    feat(page): add icon, validation

--- src/sanity/schemaTypes/documents/pageType.ts
+++ src/sanity/schemaTypes/documents/pageType.ts
@@ -1,15 +1,31 @@
 import { StickyNote } from 'lucide-react'
+import {EarthGlobeIcon} from '@sanity/icons'
+import {BlockElementIcon} from '@sanity/icons'
 import { defineField, defineType } from "sanity";
 
 export const pageType = defineType({
     name: 'page',
     title: 'Page',
     type: 'document',
+    icon: StickyNote,
+    groups: [
+        { 
+            default: true,
+            name: 'content',
+            icon: BlockElementIcon
+        }, 
+        { 
+            name: 'metadata',
+            icon: EarthGlobeIcon
+        }
+    ],
     fields: [
         defineField({
             name: 'title',
             title: 'Page Title',
             type: 'string',
+            group: 'content',
+            validation: (Rule) => Rule.required(),
         })
     ]
 })
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,12 +1,14 @@
 import type { SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './documents/categoryType'
 import { settingsPage } from './documents/settingsPage'
+import { pageType } from './documents/pageType'
 
  
 export const schema: { types: SchemaTypeDefinition[] } = {
     types: [
         // Documents
         categoryType,
+        pageType,
 
         // pages
         settingsPage,
