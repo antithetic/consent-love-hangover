97e41649712248704fe198284cce77e7f6de7eff
Raúl Garcia <ego.war@icloud.com>
2025-05-25 00:37:40 -0700

    chore(code): schema validation, friendly props

--- src/sanity/schemaTypes/documents/categoryType.ts
+++ src/sanity/schemaTypes/documents/categoryType.ts
@@ -3,15 +3,21 @@ import { defineField, defineType } from "sanity";
 
 export const categoryType = defineType({
   name: "category",
+  title: "Category",
   type: "document",
   fields: [
     defineField({
       name: "title",
+      title: "Title",
       type: "string",
+      validation: (Rule) => Rule.required().min(1).max(100),
     }),
     defineField({
       name: "description",
+      title: "Description",
       type: "text",
+      description: "A brief description of the category",
+      validation: (Rule) => Rule.required().min(1).max(500),
     }),
   ],
 });
