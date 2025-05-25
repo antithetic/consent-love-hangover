5396f6cd64ad4054bb8a7941aab2300a12072f1a
Raúl Garcia <ego.war@icloud.com>
2025-05-24 23:43:31 -0700

    chore(sanity): update sanity workspace config

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,9 @@ import { structureTool } from "sanity/structure";
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  name: 'love-hangover',
+  title: 'Love Hangover',
+  subtitle: 'consent studio',
   plugins: [structureTool()],
   schema: {
     types: [],
