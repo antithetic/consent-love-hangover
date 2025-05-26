8dc095d9c67f340667d75401a85d91cd8d980bc3
Raúl Garcia <ego.war@icloud.com>
2025-05-26 06:36:23 -0700

    fix(flyerArchive): code cleanup

--- src/sanity/schemaTypes/documents/flyerArchive.ts
+++ src/sanity/schemaTypes/documents/flyerArchive.ts
@@ -1,23 +1,4 @@
-import { defineField, defineType } from 'sanity'
-import {ImagesIcon} from '@sanity/icons'
-
-
-  
-  
-  export const flyerArchive = defineType({
-    type: "document",
-    name: "flyerArchive",
-    title: "Flyer Archive",
-    icon: ImagesIcon,
-    fields: [
-      defineField({
-        type: "string",
-        name: "title",
-        title: "Title",
-        validation: (e) => e.required(),
-      }),
 // 🌟 Update the import to pull in `defineArrayMember`
-import { defineField, defineType, defineArrayMember } from 'sanity'
 
 import { defineField, defineType, defineArrayMember } from 'sanity'
 import { ImagesIcon } from '@sanity/icons'
