4d07b613db0ac23522f330fcc613bca7bd12f977
Raúl Garcia <ego.war@icloud.com>
2025-05-26 22:22:53 -0700

    chore(deps): add react, markdoc

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,25 +2,26 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Consent Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/antithetic' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Consent Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/antithetic' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), react(), markdoc()],
 });
