184dd94ac28d8c6e95fce1cfc3184cf90794b669
Raúl Garcia <ego.war@icloud.com>
2025-05-27 07:42:46 -0700

    feat(plugin): add astro compressor

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
 
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -15,5 +17,11 @@ export default defineConfig({
     ]
   },
 
-  integrations: [react(), showTailwindcssBreakpoint()]
+  integrations: [
+    react(), 
+    showTailwindcssBreakpoint(),
+    
+    
+    // It is important that this is the last integration in the integrations property to ensure all the generated files are compressed.
+    compressor()]
 });
