d76092ac537ef0f1ef560457c7c5a51863e0913b
Raúl Garcia <ego.war@icloud.com>
2025-05-29 14:28:35 -0700

    chore(cms): add cloud config

--- apps/docs/keystatic.config.ts
+++ apps/docs/keystatic.config.ts
@@ -3,7 +3,10 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "cloud",
+  },
+  cloud: {
+    project: 'love-hangover/consent-docs',
   },
   collections: {
     posts: collection({
